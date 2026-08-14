69776455f418e3ecea0affb7cd5506a657732dbb
Mitali <mitaliibisht111@gmail.com>
2026-08-14 19:38:03 +0530

    added dyanamic parameter and query parameter and updated main.py

--- main.py
+++ main.py
@@ -10,13 +10,8 @@ def hello():
     return{"message":"I am learning FastAPI"}
 
 
-#users route
-@app.get("/users")
-def users():
-    return{
-        "users":["Mohit","Rohit","Amit"]
 
-    }
+    
 
 
 @app.get("/student")
@@ -34,4 +29,36 @@ def profile():
          "name":"Mitali",
          "course": "BCA",
          "subject":"Python"
+    }
+
+#dyanamic parameters
+@app.get("/users/{user_id}")
+def get_user(user_id:int):
+    return{"user_id": user_id}
+
+
+
+#query parameters
+#/users?name=mohit
+#/products?price=1000
+@app.get("/users")
+def get_users(name):
+    return{"Name": name}
+
+#optional parameter
+@app.get("/users")
+def get_users(name: str = None):
+    return{"Name": name}
+
+#default values
+@app.get("/products")
+def get_users(limit: int = 10):
+    return{"limit": limit}
+
+#
+@app.get("/items")
+def get_users(name:str=None,price:int=0):
+    return{
+        "name": name,
+        "price":price
     }
